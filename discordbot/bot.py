31aed27f5b23b78e17aabfa75330fa0ad4f77e0a
RenoirTan <renoirtan2005@gmail.com>
2023-01-07 15:31:31 +0800

    main: on_member_join

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -12,6 +12,8 @@ from discord.ext import commands
 # Permissions to access Discord's API
 intents = discord.Intents.default()
 intents.message_content = True # We want to see what a user has written
+intents.members = True # see members' actions
+intents.guilds = True # access to guild's stuff
 
 
 # Command prefix (e.g. "!" for !ping)
@@ -22,6 +24,16 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged on as {bot.user}")
+
+
+# When a member joins
+@bot.event
+async def on_member_join(member: discord.Member):
+    # Get default channel
+    system_channel = member.guild.system_channel
+    # Check if we are allowed to send messages
+    if system_channel.permissions_for(member.guild.me).send_messages:
+        await system_channel.send(f"<@{member.id}> has joined {member.guild.name}.")
 
 
 def run_bot(token):
