04a72702156f7b33421890f8165bee4578be390b
RenoirTan <renoirtan2005@gmail.com>
2023-03-08 23:06:44 +0800

    template: remove actual code

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -4,12 +4,7 @@
 This module contains functions and other definitions for the discord bot.
 """
 
-from random import randint
-
-import discord
-from discord.ext import commands
-
-from discordbot.util import railfence_encode, bubble_sort, best_fit_emoji, emoji_list
+# Import discord here!
 
 
 # Permissions to access Discord's API
@@ -24,7 +19,7 @@ intents.emojis_and_stickers = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-async def send_error(ctx: commands.Context, message: str):
+async def send_error(ctx, message):
     """
     Create an embedded message for an error and then send it.
     """
@@ -42,170 +37,63 @@ async def on_ready():
 
 # When a member joins
 @bot.event
-async def on_member_join(member: discord.Member):
-    # Get default channel
-    system_channel = member.guild.system_channel
-    # Check if we are allowed to send messages
-    if system_channel.permissions_for(member.guild.me).send_messages:
-        await system_channel.send(f"<@{member.id}> has joined {member.guild.name}.")
+async def on_member_join(member):
+    pass
 
 
 # Feature 1
 # Ping the bot
 @bot.command()
 async def ping(ctx):
-	await ctx.send('Pong')
+	pass
 
 
 # Feature 2
 # Calculate the result of an operation on 2 numbers.
 # Example: !calc 1 + 2
 @bot.command()
-async def calc(ctx: commands.Context, arg1: str, operator: str, arg2: str):
-    try:
-        num1 = float(arg1)
-    except:
-        await send_error(ctx, "Your first number is not a number.")
-        return
-    try:
-        num2 = float(arg2)
-    except:
-        await send_error(ctx, "Your second number is not a number.")
-        return
-    
-    if operator == "+":
-        result = num1 + num2
-    elif operator == "-":
-        result = num1 - num2
-    elif operator == "*":
-        result = num1 * num2
-    elif operator == "/":
-        result = num1 / num2
-    else:
-        await send_error(ctx, "We don't allow that kind of operation yet.")
-        return
-
-    await ctx.send(f"{arg1} {operator} {arg2} = {result}")
+async def calc(ctx, arg1, operator, arg2):
+    pass
 
 
 # Feature 3
 # Guess the number (integer)
 @bot.command()
-async def guess(ctx: commands.Context, guess: str):
-    # number = 7
-    number = randint(0, 9) # 0 to 9 inclusive
-    try:
-        guess = int(guess)
-    except:
-        await send_error(ctx, "guess is not an integer")
-        return
-    if guess < number:
-        await ctx.send("too low")
-    elif guess > number:
-        await ctx.send("too high")
-    else: # guess == number
-        await ctx.send("yay")
+async def guess(ctx, guess):
+    pass
 
 
 # Feature 4
 # Reply to member with what they said
 @bot.command()
-async def parrot(ctx: commands.Context, *args: str):
-    message = ctx.message.content[8:] # get rid of "!parrot "
-    # we get an error if we try to send a blank message, so we have to handle
-    # that case separately
-    if len(message) == 0:
-        await send_error(ctx, "You sent an empty message!")
-    else:
-        await ctx.send(message)
+async def parrot(ctx, *args):
+    pass
 
 
 # Feature 5
 @bot.command()
-async def fizzbuzz(ctx: commands.Context, limit: str):
-    try:
-        limit = int(limit)
-    except:
-        await send_error(ctx, "Limits must be a positive integer")
-        return
-    if limit <= 0:
-        await send_error(ctx, "Limits must be a positive integer")
-        return
-    
-    message = ""
-    for i in range(limit):
-        message += str(i)
-        if i % 3 == 0:
-            message += "Fizz"
-        if i % 5 == 0:
-            message += "Buzz"
-        message += "\n"
-    await ctx.send(message)
+async def fizzbuzz(ctx, limit):
+    pass
 
 
 # Feature 6
 # Sort a list of numbers using bubble sort
 @bot.command()
-async def bubble(ctx: commands.Context, *numbers: str):
-    sequence = []
-    for number in numbers:
-        try:
-            number = float(number)
-        except:
-            await send_error(ctx, f"{number} is not a number")
-            return
-        sequence.append(number)
-    if len(sequence) == 0:
-        await send_error(ctx, "Nothing to sort")
-        return
-
-    sequence = bubble_sort(sequence)
-    s_sequence = list(map(str, sequence))
-    await ctx.send("Sorted: " + " ".join(s_sequence))
+async def bubble(ctx, *numbers):
+    pass
 
 
 # Feature 7
 # Convert plaintext to crypttext using railfence encoding
 @bot.command()
-async def railfence(ctx: commands.Context, n_rails: str, *words: str):
-    try:
-        n_rails = int(n_rails)
-    except:
-        await send_error(ctx, "Rails must be a positive integer")
-        return
-    if n_rails <= 0:
-        await send_error(ctx, "Rails must be a positive integer")
-        return
-    
-    plaintext = "".join(words)
-    
-    if len(plaintext) == 0:
-        await send_error(ctx, "Your message cannot be empty")
-        return
-    
-    # See util.py
-    crypttext = railfence_encode(n_rails, plaintext)
-    
-    await ctx.send(crypttext)
+async def railfence(ctx, n_rails, *words):
+    pass
 
 
 # Feature 8
 @bot.command()
-async def emojify(ctx: commands.Context, *args: str):
-    message = ctx.message.content[9:] # get rid of "!emojify "
-    # we get an error if we try to send a blank message, so we have to handle
-    # that case separately
-    if len(message) == 0:
-        await send_error(ctx, "You sent an empty message!")
-    else:
-        words = message.split(' ') #gives an array of each word that we can iterate through
-        emojifiedreply = ''
-        options = emoji_list()
-        for word in words:
-            emojifiedreply += word #reconstructs the original message
-            bfe = best_fit_emoji(word, options)
-            emojifiedreply += " " + bfe + " " #with the emoji behind it
-        await ctx.send(emojifiedreply)
+async def emojify(ctx, *args):
+    pass
 
 
 # Run the bot
--- discordbot/util.py
+++ discordbot/util.py
@@ -1,7 +1,4 @@
-import difflib
-from .emojis import emojis
-
-def railfence_encode(n_rails: int, plaintext: str) -> str:
+def railfence_encode(n_rails, plaintext):
     """
     Encodes a plaintext string into crypttext using the railfence cipher. This
     function assumes that n_rails is at least 1.
@@ -19,111 +16,19 @@ def railfence_encode(n_rails: int, plaintext: str) -> str:
     str
         Encrypted version of plaintext (i.e. crypttext)
     """
-    # If only 1 rail, then no modification needed
-    if n_rails == 1:
-        return plaintext
-    
-    # How many characters there are in each "up-down" cycle.
-    #
-    # For example, with 5 rails:
-    # a.......i
-    # .b.....h.j
-    # ..c...g...k
-    # ...d.f.... and so on ...
-    # ....e......
-    #
-    # A letter's vertical position (a.k.a. rail) goes from top to bottom then
-    # top again (a to e to h, i is part of a new cycle).
-    # Notice there are 8 letters in the cycle.
-    # In fact, if you experiment with different numbers of rails, you'll find
-    # that the length of each cycle is 2 * (n_rails - 1)
-    cycle_len = 2*(n_rails-1)
-    
-    # List of rails
-    # Each rail stores the characters at each vertical position
-    # e.g. The 0th character falls in rails[0], the 1st character in rails[1]
-    # and so on
-    rails = []
-    for _ in range(n_rails):
-        rails.append("")
-    
-    # Go through each character in plaintext
-    for index in range(len(plaintext)):
-        # Get the character at that index
-        character = plaintext[index]
-        
-        # Figure out position of the character in the "up-down" cycle
-        # 
-        # In the example above, the letter j (index 9) would be in position 1,
-        # while for the letter h (index 7) would be in position 7.
-        #
-        # Note that `a % b` finds the remainder when `a` is divided by `b`
-        position = index % cycle_len
-        
-        # Find which rail the letter belongs to based on their position in the
-        # "up-down" cycle
-        #
-        # If position is on the downward side of the cycle (0 to 4 in the
-        # example), then the rail that character belongs to is simply its
-        # position in the cycle.
-        #
-        # However, for characters on the upward side of the cycle (from 5 to 7),
-        # the rail a character belongs to is not simply its position.
-        # For example, with the letter h (position 7), it is supposed to be in
-        # rail 1, so we must map position 7 to rail 1 like so:
-        if position >= n_rails:
-            rail = cycle_len - position
-        else:
-            rail = position
-            
-        # Add character to that rail
-        rails[rail] += character
-    
-    # Concatenate rails to create the final output
-    output = ""
-    for rail in rails:
-        output += rail
-    
-    # Send output back to calling function
-    return output
+    pass
 
 
 def bubble_sort(sequence):
     """
     Sort a sequence in ascending order using the bubble sort algorithm.
     """
-    seq_len = len(sequence)
-    ordered = False
-    
-    # Continue sorting until the list is sorted
-    while not ordered:
-        # Assume sequence is already sorted
-        ordered = True
-        # Go through each number in the sequence
-        for i in range(seq_len-1):
-            # If this number is greater than the number after it, then swap
-            if sequence[i] > sequence[i+1]:
-                # The sequence was not already ordered correctly
-                # So we set ordered to False to tell the while loop
-                # to go through the list one more time to check
-                ordered = False
-                temp = sequence[i]
-                sequence[i] = sequence[i+1]
-                sequence[i+1] = temp
-    
-    # Technically not necessary as lists are "pass by reference" in Python
-    # See https://realpython.com/python-pass-by-reference/ to find out more
-    return sequence
+    pass
 
 
 def emoji_list():
-    return list(emojis.keys())
+    pass
 
 
 def best_fit_emoji(word, options):
-    possibilities = difflib.get_close_matches(word, options, 1, 0.6)
-    if possibilities != []:
-        emoji_name = possibilities[0]
-        return emojis[emoji_name]
-    else:
-        return ''
+    pass
